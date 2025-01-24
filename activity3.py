9f0663c0e36935e2cb4c1d9783840984aa2218fe
whostausif <tausifwali@gmail.com>
2025-01-24 19:54:18 +0600

    Lesson 3 Activity 3

---
+++ activity3.py
@@ -0,0 +1,9 @@
+math = int(input("math marks:"))
+english = int(input("english marks:"))
+science = int(input("science marks:"))
+bengali = int(input("bengali marks:"))
+
+sum = math+english+science+bengali
+divide = (sum/400)
+perc = (divide*100)
+print(perc)
